16dee3c2972ed941df69a628d23261a6b69cfce4
ElielRegiani <elielregiani@gmail.com>
2026-03-24 22:45:32 -0300

    first commit

---
+++ whatsapp-integration/src/main/kotlin/whatsapp_platform/whatsapp_integration/WhatsappIntegrationApplication.kt
@@ -0,0 +1,11 @@
+package whatsapp_platform.whatsapp_integration
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WhatsappIntegrationApplication
+
+fun main(args: Array<String>) {
+	runApplication<WhatsappIntegrationApplication>(*args)
+}
---
+++ whatsapp-integration/src/test/kotlin/whatsapp_platform/whatsapp_integration/WhatsappIntegrationApplicationTests.kt
@@ -0,0 +1,13 @@
+package whatsapp_platform.whatsapp_integration
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WhatsappIntegrationApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
